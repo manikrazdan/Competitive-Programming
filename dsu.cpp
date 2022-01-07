065aaab8ca2ba33693b4ecb4643f1e0de1920942
Dev Karan Singh <85109774+devkaran7@users.noreply.github.com>
2022-01-07 15:34:10 +0530

    Create dsu.cpp

---
+++ dsu.cpp
@@ -0,0 +1,54 @@
+class dsu{
+    vector<int> parent, size, rank;
+
+public:
+    dsu(int n){
+        for(int i = 0; i <= n; i++){
+            size.push_back(1);
+            parent.push_back(i);
+            rank.push_back(0);
+        }
+    }
+
+    int findpar(int node){
+        if(parent[node] == node){
+            return node;
+        }
+
+        return parent[node] = findpar(parent[node]);
+    }
+
+    void unionsize(int u, int v){
+        int pu = findpar(u);
+        int pv = findpar(v);
+
+        if(pu == pv){
+            return;
+        }
+
+        if(size[pu] < size[pv]){
+            parent[pu] = pv;
+            size[pv] += size[pu];
+        }else{
+            parent[pv] = pu;
+            size[pu] += size[pv];
+        }
+    }
+
+    void unionrank(int u, int v){
+        int pu = findpar(u);
+        int pv = findpar(v);
+
+        if(pu == pv){
+            return;
+        }
+        if(rank[pu] == rank[pv]){
+            parent[pu] = pv;
+            rank[pu]++;
+        }else if(rank[pu] < rank[pv]){
+            parent[pu] = pv;
+        }else{
+            parent[pv] = pu;
+        }
+    }
+};
