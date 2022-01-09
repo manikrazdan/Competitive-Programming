bd742ac6839b90e6827e97cc442d794dee9481f8
Dev Karan Singh <85109774+devkaran7@users.noreply.github.com>
2022-01-09 16:53:33 +0530

    Create fenwicktree.cpp

---
+++ fenwicktree.cpp
@@ -0,0 +1,30 @@
+class fenwicktree{
+    vector<int> fen;
+    int n;
+public:
+    fenwicktree(int x){
+        fen.resize(x+1);
+        n = x;
+    }
+
+public:
+    void update(int ind, int val){
+        while(ind <= n){
+            fen[ind] += val;
+            ind += ind & (-ind);
+        }
+    }
+public:
+    int sum(int ind){
+        int s = 0;
+        while(ind >= 1){
+            s += fen[ind];
+            ind -= ind & (-ind);
+        }
+        return s;
+    }
+public:
+    int answerquery(int l, int r){
+        return sum(r) - sum(l - 1);
+    }
+};
