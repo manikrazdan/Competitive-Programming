12beb54e20406c75ec9a391ba6fac2f54b668b9c
Dev Karan Singh <85109774+devkaran7@users.noreply.github.com>
2022-09-16 23:45:42 +0530

    Update flat_tree.cpp

--- TREES/flat_tree.cpp
+++ TREES/flat_tree.cpp
@@ -1,1 +1,17 @@
-
+//flat tree ->
+ll cnt = 0;
+vector<ll> flatTree;
+ 
+void dfs(ll node, ll par, vector<ll> adj[], vector<pair<ll, ll>> &index){
+    index[node].ff = cnt;
+    flatTree.pb(node);
+    cnt++;
+    for(auto it : adj[node]){
+        if(it != par){
+            dfs(it, node, adj, index);
+        }
+    }
+    index[node].ss = cnt;
+    flatTree.pb(node);
+    cnt++;
+}
